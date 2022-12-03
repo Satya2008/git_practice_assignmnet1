861f74905816cd1a6e927aa3490487e4726d41b2
Satya2008 <satyanand807@gmail.com>
2022-12-03 19:43:58 +0530

    problem

---
+++ test.js
@@ -0,0 +1,29 @@
+1 Prime no...
+let num=7;
+let count=0;
+for(let i=0; i<=num; i--){
+if(num%i==0){
+count++
+
+}
+
+}
+if(count==2){
+console.log("prime")
+}else{
+console.log("not prime")
+}
+
+
+2. palindrome....
+let str="naman";
+let bag=0;
+for(let i=str.length-1; i>=0; i--){
+bag+=str[i];
+}
+if(str==bag){
+console.log("it is palindrome")
+}else{
+console.log("it is mot palindrome")
+
+}
